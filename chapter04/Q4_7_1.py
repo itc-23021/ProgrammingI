f0146418b00f97ec6744d56d86f4434692f17557
shingo <s23021@std.it-college.ac.jp>
2023-07-27 07:38:36 +0000

    .

---
+++ chapter04/Q4_7_1.py
@@ -0,0 +1,8 @@
+def make_addfunc()
+
+
+def add(x, y):
+    return x+y
+
+
+return add
---
+++ chapter04/kuku.py
@@ -0,0 +1,5 @@
+for i in range(1, 10):
+    for c in range(1, 10):
+        ans = i * c
+        print(f"{ans:>4}", end="")
+    print()
