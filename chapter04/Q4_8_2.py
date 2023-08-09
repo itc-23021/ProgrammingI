8674d7daccc6146696f98149da9f2570f1fb0c37
shingo <s23021@std.it-college.ac.jp>
2023-08-09 05:18:52 +0000

    .

---
+++ chapter04/Q4_8_2.py
@@ -0,0 +1,10 @@
+import calendar
+
+print(calendar.month(2023, 8))
+print(type(calendar.month(2023, 1)))
+color_code = {
+    "holiday": "\033[91m",
+    "sunday": "\033[91m",
+    "saturday": "\033[94m",
+    "end": "\033[0m",
+}
---
+++ chapter05/Q5_1_1.py
@@ -0,0 +1,3 @@
+members = ["Yamada", "tanaka", "Satou", "Suzuki"]
+name = members.pop(3)
+print(name)
