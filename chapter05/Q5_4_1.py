dd9e8cc79c8b592b9f56a98beb6ffcda17b27f72
shingo <s23021@std.it-college.ac.jp>
2023-08-24 05:38:21 +0000

    .

---
+++ chapter05/Q5_4_1.py
@@ -0,0 +1,4 @@
+a, b, c, d, e = ('Sapporo', 'Sendai', Tokyo','Nagoya', 'Osaka')
+g = d+e
+g
+# 'NagoyaOsaka'が出力される
--- chapter05/Q5_7_1.py
+++ chapter05/Q5_7_1.py
@@ -1,3 +1,4 @@
 yamanote_line = {0: "tokyo", 1: "Yuurakuchou", 2: "Shibnashi"}
 for v in yamanote_line.values():
     print(v)
+    # 順不同 TokyoYuurakuchou Shinbashiと出力される
